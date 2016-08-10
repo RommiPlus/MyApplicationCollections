627cc79a47035c13dc8c49434c7ece5bdb5657fd
RommiPlus <junqingt@gmail.com>
2016-08-10 23:24:25 +0800

    fea:  add click message

--- app/src/main/java/com/myapplicationcollections/MainActivity.java
+++ app/src/main/java/com/myapplicationcollections/MainActivity.java
@@ -1,13 +1,49 @@
 package com.myapplicationcollections;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mMessage = " message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToastPopularMovie(View view) {
+        showToast(makeMessage(getString(R.string.popular_movies)));
+    }
+
+    public void showToastStockHawk(View view) {
+        showToast(makeMessage(getString(R.string.stock_hawk)));
+    }
+
+    public void showToastBuildBigger(View view) {
+        showToast(makeMessage(getString(R.string.build_bigger)));
+    }
+
+    public void showToastMakeMaterial(View view) {
+        showToast(makeMessage(getString(R.string.make_material)));
+    }
+
+    public void showToastGoUbiquitous(View view) {
+        showToast(makeMessage(getString(R.string.go_ubiquitous)));
+    }
+
+    public void showToastCapstone(View view) {
+        showToast(makeMessage(getString(R.string.capstone)));
+    }
+
+    public String makeMessage(String message) {
+        return message + mMessage;
+    }
+
+    public void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
